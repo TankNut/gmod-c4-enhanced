28ef554a18d384621548a97fec9a6603b24c5de6
TankNut <m.vlasblom@live.nl>
2022-07-25 03:15:16 +0200

    Add convars to readme

--- README.md
+++ README.md
@@ -5,5 +5,11 @@ An addon for Garry's Mod that adds C4 and a matching defuse kit.
 
 ## Features
 * Custom timers
-* Configurable min-max time, damage and defuse time
+* Configurable min-max explosion timers, damage and defuse duration
 * Gbombs 5 sounds and effects
+
+## Convars
+* `c4_enhanced_mintimer` - The minimum timer C4 can be set to.
+* `c4_enhanced_maxtimer` - The maximum timer C4 can be set to.
+* `c4_enhanced_damage` - How much damage C4 does when it explodes.
+* `c4_enhanced_defusetimer` - How long it takes to defuse C4.
--- lua/entities/ent_c4_enhanced/shared.lua
+++ lua/entities/ent_c4_enhanced/shared.lua
@@ -2,7 +2,7 @@ AddCSLuaFile()
 
 CreateConVar("c4_enhanced_mintimer", 0, {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "The minimum timer C4 can be set to.", 0, 3599)
 CreateConVar("c4_enhanced_maxtimer", 3599, {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "The maximum timer C4 can be set to.", 0, 3599)
-CreateConVar("c4_enhanced_damage", 400, {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "How much damage C4 does when it explodes")
+CreateConVar("c4_enhanced_damage", 400, {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "How much damage C4 does when it explodes.")
 
 local convarMin = GetConVar("c4_enhanced_mintimer")
 local convarDamage = GetConVar("c4_enhanced_damage")
